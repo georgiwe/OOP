e884655562743fc2be32234015acba4eaa26d7fd
Georgi Prodanov <georgiwe@gmail.com>
2014-02-09 01:17:24 +0200

    fixes

--- HW - PrinciplesOfOOP1/AnimalKingdom/Animal.cs
+++ HW - PrinciplesOfOOP1/AnimalKingdom/Animal.cs
@@ -1,5 +1,8 @@
 ﻿namespace AnimalKingdom
 {
+    using System.Collections.Generic;
+    using System.Linq;
+
     public abstract class Animal : ISound
     {
         public Animal(string name, int age, bool isMale)
@@ -15,9 +18,19 @@
 
         public bool IsMale { get; private set; }
 
+        public static double GetAvgAge(IEnumerable<Animal> arr)
+        {
+            return (double)arr.Select(x => x.Age).Aggregate((x, y) => x + y) / arr.Count();
+        }
+
         public virtual void MakeSound()
         {
             System.Console.WriteLine("Animal sound!");
+        }
+
+        public override string ToString()
+        {
+            return "Animal sound!";
         }
     }
 }
--- HW - PrinciplesOfOOP1/AnimalKingdom/Launch.cs
+++ HW - PrinciplesOfOOP1/AnimalKingdom/Launch.cs
@@ -8,46 +8,41 @@
     {
         public static void Main()
         {
-            Dog[] dogArr = new Dog[]
+            Dog[] dogArr =
             {
                 new Dog("Sharo", 4, true),
                 new Dog("Balkan", 5, true),
                 new Dog("La Chupacabra", 1, false)
             };
 
-            Kitten[] kittens = new Kitten[]
+            Kitten[] kittens =
             {
                 new Kitten("Kittycat", 3),
                 new Kitten("Pussycat", 2),
                 new Kitten("Fluff", 4),
             };
 
-            Tomcat[] tomcats = new Tomcat[]
+            Tomcat[] tomcats =
             {
                 new Tomcat("Tom", 5),
                 new Tomcat("Silvester", 4),
                 new Tomcat("Topcat", 4)
             };
 
-            Frog[] frogs = new Frog[]
+            Frog[] frogs =
             {
                 new Frog("Frogger", 1, true),
                 new Frog("Frogmeister", 1, true),
                 new Frog("Frogette", 1, true)
             };
 
-            double dogAvgAge = GetAvgAge(dogArr);
-            double kittAvgAge = GetAvgAge(kittens);
-            double frgAvgAge = GetAvgAge(frogs);
+            double dogAvgAge = Animal.GetAvgAge(dogArr);
+            double kittAvgAge = Animal.GetAvgAge(kittens);
+            double frgAvgAge = Animal.GetAvgAge(frogs);
 
             Console.WriteLine(dogAvgAge);
             Console.WriteLine(kittAvgAge);
             Console.WriteLine(frgAvgAge);
-        }
-
-        private static double GetAvgAge(IEnumerable<Animal> arr)
-        {
-            return (double)arr.Select(x => x.Age).Aggregate((x, y) => x + y) / arr.Count();
         }
     }
 }
