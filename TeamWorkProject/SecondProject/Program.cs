075764c65807a7441b015c748b9454b853974641
Georgi Prodanov <georgiwe@gmail.com>
2014-02-10 10:53:13 +0200

    First Commit

---
+++ TeamWorkProject/SecondProject/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("2nd try");
+        }
+    }
+}
